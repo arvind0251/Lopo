ebf911c40ad2d4169be465c100167cca3b08900e
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2025-06-04 20:06:01 +0530

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,10 +1,10 @@
 import ta
 
 def add_indicators(df):
-    df['rsi'] = ta.momentum.rsi(df['close'], window=14)
-    df['macd'] = ta.trend.macd_diff(df['close'])
-    df['ema_21'] = ta.trend.ema_indicator(df['close'], window=21)
-    df['ema_50'] = ta.trend.ema_indicator(df['close'], window=50)
-    df['bb_high'] = ta.volatility.bollinger_hband(df['close'], window=20)
-    df['bb_low'] = ta.volatility.bollinger_lband(df['close'], window=20)
+    df['rsi'] = ta.momentum.rsi(df['close'], window=14, fillna=True)
+    df['macd'] = ta.trend.macd_diff(df['close'], fillna=True)
+    df['ema_21'] = ta.trend.ema_indicator(df['close'], window=21, fillna=True)
+    df['ema_50'] = ta.trend.ema_indicator(df['close'], window=50, fillna=True)
+    df['bb_high'] = ta.volatility.bollinger_hband(df['close'], window=20, fillna=True)
+    df['bb_low'] = ta.volatility.bollinger_lband(df['close'], window=20, fillna=True)
     return df
